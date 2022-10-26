5110d19f82921d86f5741264f6bd323de62c0655
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2022-10-26 02:42:47 -0500

    Clase main, no funciona

---
+++ Main.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "ArrayStack.h"
+using namespace std;
+
+int main(){
+  //ArrayStack<int> stack(10);
+
+  ArrayStack<int> * pstack = new ArrayStack<int>(10);
+
+  //pstack->push(1);
+  return 0;
+}
