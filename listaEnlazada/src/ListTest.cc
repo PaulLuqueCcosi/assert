2ada922a61bfb89a3c60038b15ffbb92ec845c84
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2022-11-02 16:46:38 -0500

    corregiendo listaEnlazada

--- listaEnlazada/src/ListTest.cc
+++ listaEnlazada/src/ListTest.cc
@@ -2,7 +2,18 @@
 #include "gtest/gtest.h"
 
 TEST(ListTest, EmptyList) {
-	List *list = new List();
-	EXPECT_EQ(0, list->size());
-	EXPECT_TRUE(list->isEmpty());
+  List *list = new List();
+  EXPECT_EQ(0, list->size());
+  EXPECT_TRUE(list->isEmpty());
+}
+
+TEST(ListTest, append) {
+  List *list = new List();
+  list->append(1);
+  EXPECT_EQ(1, list->size());
+  EXPECT_FALSE(list->isEmpty());
+
+  list->append(2, 0);
+  EXPECT_EQ(2, list->size());
+  EXPECT_EQ(2, list->head());
 }
--- listaEnlazada/src/list.cc
+++ listaEnlazada/src/list.cc
@@ -4,110 +4,120 @@ using namespace std;
 // Constructor
 List::List()
 {
-	first = nullptr;
+  first = nullptr;
 }
 
 // Agregar un dato
 List *List::append(int e)
 {
-	if (isEmpty())
-	{ // caso inicial
-		first = new Node;
-		first->data = e;
-		first->next = nullptr;
-		return this;
-	}
-	Node *n = new Node;
-	n->data = e;
-	// probando con la nueva funcione
+  if (isEmpty())
+  { // caso inicial
+    first = new Node;
+    first->data = e;
+    first->next = nullptr;
+    return this;
+  }
+  Node *n = new Node;
+  n->data = e;
+  // probando con la nueva funcione
 
-	addNext(first, n);
+  addNext(first, n);
 
-	// n->next = last->next;
-	// last->next = n;
+  // n->next = last->next;
+  // last->next = n;
 
-	return this;
+  return this;
 }
 
 // obtenemos la cabeza
 int List::head()
 {
-	return  first->data;
+  return  first->data;
 }
 
 // obtenemos el tamanio
 int List::size()
 {
-	int c = 0;
-	Node *iter = first;
-	while (iter)
-	{
-	  iter = iter->next;
-	  c++;
-	}
-	return c;
+  int c = 0;
+  Node *iter = first;
+  while (iter)
+  {
+    iter = iter->next;
+    c++;
+  }
+  return c;
 }
 
 // verificamos si esta vacio
 bool List::isEmpty()
 {
-	return first == nullptr;
+  return first == nullptr;
 }
 
 
 // Agregar un dato en una posicion
 List *List::append(int data, int pos)
 {
-	if (isEmpty())
-	{ // caso inicial
-		first = new Node;
-		first->data = data;
-		first->next = nullptr;
-		return this;
-	}
+  if (isEmpty())
+  { // caso inicial
+    first = new Node;
+    first->data = data;
+    first->next = nullptr;
+    return this;
+  }
 
-	// verificamos si entraria en esas posicion
-	// aun no consideramos lista circular
-	if (pos > size())
-	{
-		cout << "La posicion no existe" << endl;
-		return this;
-	}
+  // verificamos si entraria en esas posicion
+  // aun no consideramos lista circular
+  if (pos > size())
+  {
+    cout << "La posicion no existe" << endl;
+    return this;
+  }
 
-	// creamos un puntero para recorrer
-	Node *iter = first;
-	while (pos--)
-	{
-		iter = iter->next;
-	}
+  // posicion 0
+  if (pos == 0)
+  {
+    Node *n = new Node;
+    n->data = data;
+    n->next = first;
+    first = n;
+    return this;
+  }
 
-	// creamos el nodo
-	Node *n = new Node;
-	n->data = data;
+  // creamos un puntero para recorrer
+  Node *iter = first;
+  while (pos--)
+  {
+    iter = iter->next;
+  }
 
-	// agregamos el nodo
-	addNext(iter, n);
+  // creamos el nodo
+  Node *n = new Node;
+  n->data = data;
 
-	return this;
+  // agregamos el nodo
+  addNext(iter, n);
+
+  return this;
 }
 
 Node *List::deleteNext(Node *current)
 {
-	if (current == nullptr)
-	{
-		return current;
-	}
-	current->next = current->next->next;
-	return current;
+  if (current == nullptr)
+  {
+    return current;
+  }
+  current->next = current->next->next;
+  return current;
 }
 
 Node *List::addNext(Node *current, Node *newNode)
 {
-	if (current == nullptr)
-		return newNode;
+  if (current == nullptr)
+    return newNode;
 
-	newNode->next = current->next;
-	current->next = newNode;
+  newNode->next = current->next;
+  current->next = newNode;
 
-	return newNode;
+  return newNode;
 }
